f38d042a027b9ae0199e523d93db5980c5315ad7
nolan <nericks2@charlotte.edu>
2026-04-16 17:02:51 -0400

    Backpack class declaration

---
+++ Domain/Backpack.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class Backpack : Gear
+{
+    public int Volume { get; set; }
+
+    public Backpack(string name, double price, double weight, string link, int volume)
+        : base(name, price, weight, GearType.Backpack, link)
+    {
+        Volume = volume;
+    }
+}
